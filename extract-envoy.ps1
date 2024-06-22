5e2baeaa8fb969ebf743e8c0434c20c1519f1560
Lan <gcslyp@gmail.com>
2024-06-22 19:14:07 +0800

    update envoy version to 1.25.11

--- extract-envoy.ps1
+++ extract-envoy.ps1
@@ -4,7 +4,7 @@ param(
 )
 
 $dockerUrl = "https://download.docker.com/win/static/stable/x86_64/docker-20.10.13.zip"
-$dockerImage = "envoyproxy/envoy-windows:v1.25.1"
+$dockerImage = "envoyproxy/envoy-windows:v1.25.11"
 
 # Check for administrator
 function Test-Admin {
